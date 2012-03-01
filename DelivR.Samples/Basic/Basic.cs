47ad076ec4969ae068bf168c0dd24124ca71c5e8
aaronpowell <me@aaron-powell.com>
2012-03-01 14:20:13 -0800

    type naming change

--- DelivR.Samples/Basic/Basic.cs
+++ DelivR.Samples/Basic/Basic.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Web;
+using SignalR.Hosting;
 
 namespace DelivR.Samples.Basic
 {
     public class Basic : FileConnection
     {
-        protected override System.Threading.Tasks.Task OnConnectedAsync(SignalR.Hosting.IRequest request, IEnumerable<string> groups, string connectionId)
+        protected override Task OnConnectedAsync(IRequest request, IEnumerable<string> groups, string connectionId)
         {
             this.SendImage(connectionId, Path.Combine(@"C:\_Projects\github\DelivR\DelivR.Samples\Basic", "signalr_18579_lg.gif"));
             return base.OnConnectedAsync(request, groups, connectionId);
